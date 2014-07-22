09afa5840cda655da68de1d6cb28398d9909e753
gong023 <gon.gong.gone@gmail.com>
2014-07-22 17:25:00 +0900

    create sandbox

---
+++ src/LibName.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * CascadeFixture.php
+ *
+ * @package
+ * @author    gong023 <gon.gong.gone@gmail.com>
+ */
+namespace CascadeFixture;
+
+/**
+ * Class LibName
+ *
+ * @package LibName
+ * @author  gong023 <gon.gong.gone@gmail.com>
+ */
+class LibName
+{
+}
---
+++ test/LibName/LibNameTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * LibName.php
+ * 
+ * @package
+ * @author    gong023 <gon.gong.gone@gmail.com>
+ * @copyright GREE, Inc.
+ */
+namespace LibName;
+
+/**
+ * Class LibName
+ *
+ * @package LibName
+ * @author  gong023 <gon.gong.gone@gmail.com>
+ */
+class LibNameTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function first()
+    {
+        $this->assertTrue(true);
+    }
+}
